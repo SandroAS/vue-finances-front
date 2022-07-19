bc505428d3b9f06820dfa39058a573e3315f966e
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2022-07-19 08:22:19 -0300

    personalizando tema

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -1,6 +1,44 @@
 <template>
-  <v-container>
-    <h1>Login</h1>
+  <v-container fill-height>
+    <v-layout justify-center align-center>
+
+      <v-flex xs12 sm6 md4 lg3 xl3>
+        <v-card elevation="12">
+
+          <v-toolbar color="primary" dark>
+            <v-toolbar-title>Login</v-toolbar-title>
+          </v-toolbar>
+
+          <v-card-text>
+            <v-form>
+              <label for="email">E-mail</label>
+              <v-text-field
+                prepend-icon="email"
+                name="email"
+                type="email"
+              ></v-text-field>
+              <label for="password">Senha</label>
+              <v-text-field
+                prepend-icon="lock"
+                name="password"
+                type="password"
+              ></v-text-field>
+            </v-form>
+            <v-btn block depressed color="secondary">
+              Criar Conta
+            </v-btn>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" large>
+              Login
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+
+    </v-layout>
   </v-container>
 </template>
 
