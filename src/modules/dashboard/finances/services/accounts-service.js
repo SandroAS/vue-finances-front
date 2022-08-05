32b14ff539ec9dca9f322ab17e9aaa58ec2b7ed9
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2022-08-05 18:06:49 -0300

    query categories

--- src/modules/dashboard/finances/services/accounts-service.js
+++ src/modules/dashboard/finances/services/accounts-service.js
@@ -1,6 +1,6 @@
 import apollo from '@/plugins/apollo'
 
-import AccountQuery from './../graphql/AccountQuery.gql'
+import AccountQuery from './../graphql/Accounts.gql'
 
 const accounts = async () => {
   const response = await apollo.query({
