91beb85bb040b4e504262e9460fa209efcbd07cf
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2023-02-08 11:09:25 -0300

    opcoes de select de categoria

--- src/modules/dashboard/finances/components/AccountCategoryAdd.vue
+++ src/modules/dashboard/finances/components/AccountCategoryAdd.vue
@@ -3,23 +3,40 @@
     <v-card-title>{{ title }}</v-card-title>
 
     <v-card-text>
-      <v-text-field label="Descrição"></v-text-field>
+
+      <v-text-field
+        label="Descrição"
+        v-model="$v.item.description.$model"
+      ></v-text-field>
       <v-select
         v-if="entity === 'category'"
-        label="Operação">
-      </v-select>
+        label="Operação"
+        v-model="$v.item.operation.$model"
+        :items="operations"
+        item-text="description"
+        item-value="value"
+      ></v-select>
+
     </v-card-text>
 
     <v-card-actions>
       <v-spacer></v-spacer>
       <v-btn flat @click="$emit('close')">Cancelar</v-btn>
-      <v-btn flat color="primary" @click="$emit('close')">Salvar</v-btn>
+      <v-btn
+        flat
+        color="primary"
+        :disabled="$v.$invalid"
+        @click="save"
+      >
+        Salvar
+      </v-btn>
     </v-card-actions>
 
   </v-card>
 </template>
 
 <script>
+import { required, minLength } from 'vuelidate/lib/validators'
 export default {
   name: 'AcountCategoryAdd',
   props: {
@@ -29,11 +46,45 @@ export default {
       default: ''
     }
   },
+  data: () => ({
+    item: {
+      description: '',
+      operation: ''
+    },
+    operations: [
+      { description: 'Receita', value: 'CREDIT' },
+      { description: 'Despesa', value: 'DEBIT' }
+    ]
+  }),
+  validations() {
+    const validations = {
+      item: {
+        description: {
+          required,
+          minLength: minLength(3)
+        }
+      }
+    }
+
+    if(this.entity === 'account') { return validations }
+
+    return {
+      item: {
+        ...validations.item,
+        operation: { required }
+      }
+    }
+  },
   computed: {
     title() {
       return this.entity === 'account'
         ? 'Nova Conta'
         : 'Nova Categoria'
+    }
+  },
+  methods: {
+    save(e) {
+      console.log('Item: ', this.item)
     }
   }
 }
