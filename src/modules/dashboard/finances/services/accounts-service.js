a2d0cacd52f4de61d2e35cfd97675d489d2d77ee
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2023-02-08 11:24:32 -0300

    createAccount

--- src/modules/dashboard/finances/services/accounts-service.js
+++ src/modules/dashboard/finances/services/accounts-service.js
@@ -1,14 +1,40 @@
 import apollo from '@/plugins/apollo'
 
-import AccountQuery from './../graphql/Accounts.gql'
+import AccountsQuery from './../graphql/Accounts.gql'
+import AccountCreateMutation from './../graphql/AccountCreateMutation.gql'
 
 const accounts = async () => {
   const response = await apollo.query({
-    query: AccountQuery
+    query: AccountsQuery
   })
   return response.data.accounts
 }
 
+const createAccount = async variables => {
+  const response = await apollo.mutate({
+    mutation: AccountCreateMutation,
+    variables,
+    update: (proxy, { data: { createAccount} }) => {
+      try {
+        const data = proxy.readQuery({
+          AccountsQuery
+        })
+
+        data.accounts = [...data.accounts, createAccount]
+
+        proxy.writeQuery({
+          query: AccountsQuery,
+          data
+        })
+      } catch(err) {
+        console.error('Query "accounts" has not been read yet!', err)
+      }
+    }
+  })
+  return response.data.createAccount
+}
+
 export default {
-  accounts
+  accounts,
+  createAccount
 }
