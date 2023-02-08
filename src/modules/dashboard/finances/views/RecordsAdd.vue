a5931a2f8fa4c909e3a50a8ec1e34d5db59eefa0
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2023-02-08 10:50:48 -0300

    adicionando formulario no dialog em um component separado

--- src/modules/dashboard/finances/views/RecordsAdd.vue
+++ src/modules/dashboard/finances/views/RecordsAdd.vue
@@ -196,9 +196,11 @@
           v-model="showAccountCategoryDialog"
           max-width="350px"
         >
-          <v-card>
-            <v-card-title>Account or Category</v-card-title>
-          </v-card>
+          <AccountCategoryAdd
+            v-if="showAccountCategoryDialog"
+            :entity="entity"
+            @close="showAccountCategoryDialog = false"
+          />
         </v-dialog>
 
       </v-flex>
@@ -211,6 +213,7 @@ import moment from 'moment'
 import { decimal, minLength, required } from 'vuelidate/lib/validators'
 import { mapActions } from 'vuex'
 
+import AccountCategoryAdd from './../components/AccountCategoryAdd.vue'
 import AccountsService from './../services/accounts-service'
 import CategoriesService from './../services/categories-service'
 import NumericDisplay from './../components/NumericDisplay.vue'
@@ -218,7 +221,7 @@ import RecordsService from './../services/records-service'
 
 export default {
   name: 'RecordsAdd',
-  components: { NumericDisplay },
+  components: { NumericDisplay, AccountCategoryAdd },
   data() {
     return {
       accounts: [],
